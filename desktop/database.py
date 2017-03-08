5c4b48fe90679b936d3ad0219380797f10e7f3dd
Justin Trainor <js952638@dal.ca>
2017-03-08 12:10:35 -0400

    Adjust database to use PHP script rather than direct access

--- desktop/database.py
+++ desktop/database.py
@@ -1,85 +1,93 @@
-from sqlalchemy.orm import sessionmaker, relationship
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.exc import OperationalError
-from sqlalchemy.dialects.mysql import DOUBLE, SMALLINT, VARCHAR, TIMESTAMP, INTEGER
-from sqlalchemy import Column, ForeignKey, create_engine
-
-# Database information
-USERNAME = "curling"
-PASSWORD = "tit2kta;cpd]Ka3fHQus"
-HOSTNAME = "outreach.cs.dal.ca"
-PORT = 3306
-DB_NAME = "curling"
-URI = "mysql://{0}:{1}@{2}:{3}/{4}".format(USERNAME, PASSWORD, HOSTNAME, PORT, DB_NAME)
-
-# Database connection engine
-engine = create_engine(URI, echo=False)
-Session = sessionmaker(bind=engine)
-
-# Base class for table mappings
-Base = declarative_base()
+import json
+import requests
 
 
-def save(table_object):
-    """
-    Saves to the database
+API_URL = "https://outreach.cs.dal.ca/curling/api"
 
-    Args:
-        table_object: an instance of a class that is mapped to a table in the database
 
-    Throws:
-        TypeError: if class type cannot be saved
-        (Also SqlAlchemy errors)
-
-    Returns:
-        Primary key of saved object
-    """
-    session = Session()
-    session.add(table_object)
-    session.commit()
-
-    if isinstance(table_object, DemoSession):
-        return table_object.id
-    elif isinstance(table_object, RawData) or isinstance(table_object, ProcessedData):
-        return table_object.timestamp
+# Method for saving a demo session
+def saveDemoSession(session):
+    r = requests.post(API_URL + "/insert-demo.php", data={ "session": session.as_json_string() }).json()
+    if (r["error"]):
+        raise RuntimeError("Couldn't save session")
     else:
-        raise TypeError("Class type cannot be saved")
+        return r["sessionId"]
 
 
-def save_all(table_objects):
-    """
-    Saves iterable of objects to the database
+# Base class for objects that can be converted to JSON string
+class JsonBase():
+    def __init__(self):
+        self.values = {}
 
-    Args:
-        table_object: an iterable containing instances of classes that are mapped to a table in the database
-    """
-    session = Session()
-    for table_object in table_objects:
-        session.add(table_object)
-    session.commit()
+    def add_if_not_none(self, key, value):
+        if value is not None:
+            self.values[key] = value
 
-
-def print_instances(table_class):
-    """
-    Prints the attributes of each instance in table_class
-
-    Example:
-        > print_instances(DemoSession)
-    """
-    try:
-        session = Session()
-        for instance in session.query(table_class):
-            print(vars(instance))
-
-    except OperationalError as e:
-        print(e)
+    def as_json_string(self):
+        return json.dumps(self.values)
 
 
-class DemoSession(Base):
-    __tablename__ = 'Session'
+# Represents a raw data point
+class RawDataPoint(JsonBase):
+    def __init__(self,
+                 timestamp,
+                 session_id,
+                 head_ax,
+                 head_ay,
+                 head_az,
+                 handle_imu_ax,
+                 handle_imu_ay,
+                 handle_imu_az,
+                 handle_imu_qx,
+                 handle_imu_qy,
+                 handle_imu_qz,
+                 strain_gauge_1,
+                 strain_gauge_2,
+                 strain_gauge_3,
+                 strain_gauge_4,
+                 v_reference):
+        JsonBase.__init__(self)
 
+        self.values["timestamp"] = timestamp
+        self.values["headAx"] = head_ax
+        self.values["headAy"] = head_ay
+        self.values["headAz"] = head_az
+        self.values["handleImuAx"] = handle_imu_ax
+        self.values["handleImuAy"] = handle_imu_ay
+        self.values["handleImuAz"] = handle_imu_az
+        self.values["handleImuQx"] = handle_imu_qx
+        self.values["handleImuQy"] = handle_imu_qy
+        self.values["handleImuQz"] = handle_imu_qz
+        self.values["strainGauge1"] = strain_gauge_1
+        self.values["strainGauge2"] = strain_gauge_2
+        self.values["strainGauge3"] = strain_gauge_3
+        self.values["strainGauge4"] = strain_gauge_4
+        self.values["vReference"] = v_reference
+
+
+# Represents a processed data point
+class ProcessedDataPoint(JsonBase):
+    def __init__(self,
+                 timestamp,
+                 session_id,
+                 broom_angle,
+                 vertical_force,
+                 horizontal_force):
+        JsonBase.__init__(self)
+
+        self.values["timestamp"] = timestamp
+        self.values["sessionId"] = session_id
+        self.values["broomAngle"] = broom_angle
+        self.values["verticalForce"] = vertical_force
+        self.values["horizontalForce"] = horizontal_force
+
+
+# Represents an entire demo session including raw and processed data as well as metadata
+class Session(JsonBase):
     def __init__(self,
                  first_name,
+                 raw_data=None,
+                 processed_data=None,
                  last_name=None,
                  notes=None,
                  email=None,
@@ -87,109 +95,59 @@ class DemoSession(Base):
                  mean_sustained_force=None,
                  mean_brushing_force=None,
                  mean_stroke_rate=None):
-        self.firstName = first_name
-        self.lastName = last_name
-        self.notes = notes
-        self.email = email
-        self.meanMaximumForce = mean_maximum_force
-        self.meanSustainedFoce = mean_sustained_force
-        self.meanBrushingForce = mean_brushing_force
-        self.meanStrokeRate = mean_stroke_rate
+        JsonBase.__init__(self)
 
-    # Fields
-    id = Column(INTEGER(), primary_key=True, autoincrement=True)
-    timestamp = Column(TIMESTAMP(), nullable=False)
-    firstName = Column(VARCHAR(20), nullable=False)
-    lastName = Column(VARCHAR(20))
-    notes = Column(VARCHAR(255))
-    email = Column(VARCHAR(50))
-    meanMaximumForce = Column(DOUBLE())
-    meanSustainedFoce = Column(DOUBLE())
-    meanBrushingForce = Column(DOUBLE())
-    meanStrokeRate = Column(DOUBLE())
+        self.values["firstName"] = first_name
+        self.values["rawData"] = raw_data
+        self.values["processedData"] = processed_data
 
-    # Relationships
-    rawData = relationship("RawData", back_populates="session")
-    processedData = relationship("ProcessedData", back_populates="session")
+        self.add_if_not_none("lastName", last_name)
+        self.add_if_not_none("notes", notes)
+        self.add_if_not_none("email", email)
+        self.add_if_not_none("meanMaximumForce", mean_maximum_force)
+        self.add_if_not_none("meanSustainedForce", mean_sustained_force)
+        self.add_if_not_none("meanBrushingForce", mean_brushing_force)
+        self.add_if_not_none("meanStrokeRate", mean_stroke_rate)
 
+    def as_json_string(self):
+        rawDataArray = self.values["rawData"]
+        processedDataArray = self.values["processedData"]
+        self.values["rawData"] = [x.values for x in rawDataArray]
+        self.values["processedData"] = [x.values for x in processedDataArray]
+        json_str = JsonBase.as_json_string(self)
+        self.values["rawData"] = rawDataArray
+        self.values["processedData"] = processedDataArray
+        return json_str
 
-class RawData(Base):
-    __tablename__ = 'RawData'
-
-    # Fields
-    timestamp = Column(DOUBLE(), primary_key=True)
-    sessionId = Column(INTEGER(), ForeignKey('Session.id'), nullable=False)
-    headAx = Column(SMALLINT())
-    headAy = Column(SMALLINT())
-    headAz = Column(SMALLINT())
-    handleImuAx = Column(SMALLINT())
-    handleImuAy = Column(SMALLINT(), nullable=False)
-    handleImuAz = Column(SMALLINT(), nullable=False)
-    handleImuQx = Column(SMALLINT())
-    handleImuQy = Column(SMALLINT())
-    handleImuQz = Column(SMALLINT())
-    strainGauge1 = Column(SMALLINT())
-    strainGauge2 = Column(SMALLINT(), nullable=False)
-    strainGauge3 = Column(SMALLINT(), nullable=False)
-    strainGauge4 = Column(SMALLINT())
-    vReference = Column(SMALLINT())
-
-    # Relationships
-    session = relationship("DemoSession", back_populates="rawData")
-
-
-class ProcessedData(Base):
-    __tablename__ = 'ProcessedData'
-
-    def __init__(self,
-                 timestamp,
-                 session_id,
-                 broom_angle,
-                 vertical_force,
-                 horizontal_force):
-        self.timestamp = timestamp
-        self.sessionId = session_id
-        self.broomAngle = broom_angle
-        self.verticalForce = vertical_force
-        self.horizontalForce = horizontal_force
-
-    # Fields
-    timestamp = Column(DOUBLE(), primary_key=True)
-    sessionId = Column(INTEGER(), ForeignKey('Session.id'), nullable=False)
-    broomAngle = Column(DOUBLE(), nullable=False)
-    verticalForce = Column(DOUBLE(), nullable=False)
-    horizontalForce = Column(DOUBLE(), nullable=False)
-
-    # Relationships
-    session = relationship("DemoSession", back_populates="processedData")
-
-
-# Create tables
-Base.metadata.create_all(engine)
-
+    def save(self):
+        return saveDemoSession(self)
+    
 
 if __name__ == "__main__":
     # Example usage
-    demo = DemoSession("Kermit")
-    id = save(demo)
-    print(id)
-    raw_data = RawData(timestamp=23,
-                       sessionId=id,
-                       headAx=12,
-                       headAy=13,
-                       headAz=-2,
-                       handleImuAx=2000,
-                       handleImuAy=-2000,
-                       handleImuAz=0,
-                       handleImuQx=1212,
-                       handleImuQy=1409,
-                       handleImuQz=65,
-                       strainGauge1=15,
-                       strainGauge2=16,
-                       strainGauge3=17,
-                       strainGauge4=18,
-                       vReference=2012)
+    processed_data_points = [
+        ProcessedDataPoint(1, 1, 12, 13, 14), 
+        ProcessedDataPoint(2, 1, 13, 14, 15)
+    ]
 
-    processed_data = ProcessedData(23, id, 12, 15, 18)
-    print(save(raw_data))
-    print(save(processed_data))
+    raw_data_points = [
+        RawDataPoint(1, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14),
+        RawDataPoint(2, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
+    ]
+
+    session = Session('Aleysha',
+                      raw_data_points,
+                      processed_data_points,
+                      'Mullen',
+                      'These are my notes',
+                      'email@email.com',
+                      10,
+                      20,
+                      30,
+                      40)
+
+    try:
+        id = session.save()
+        print('Success! Saved session with id: ' + str(id))
+    except RuntimeError as error:
+        print(error)
