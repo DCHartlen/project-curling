66eaf39d7c7ca78c68fd8f73a711125d74f19adc
Ryan Gosse <ryan.s.gosse@gmail.com>
2017-04-04 21:58:48 -0300

    Added reminder to switch connections

--- desktop/ui.py
+++ desktop/ui.py
@@ -36,6 +36,8 @@ class UI(QtGui.QWidget):
         global lNameEdit
         global notes
         global btnDiscard
+        global msgBox
+
 
         #creating buttons, labels, textboxes.
         self.setWindowIcon(QtGui.QIcon(os.path.join('images', 'disconnected.png')))
@@ -62,6 +64,9 @@ class UI(QtGui.QWidget):
         notes = QtGui.QTextEdit()
         fNameEdit = QtGui.QLineEdit()
         lNameEdit = QtGui.QLineEdit()
+        msgBox = QtGui.QMessageBox()
+        msgBox.setText("Remember to change your network connection!")
+
 
         #fonts
         titleFont = QtGui.QFont()
@@ -186,11 +191,11 @@ class UI(QtGui.QWidget):
             stats = BrushingStatistics([x.values["horizontalForce"] for x in processed_data_points], 10.0) # demo is currently 10.0 seconds
 
             try:
-                session = Session(fNameEdit.text(), 
-                                  raw_data_points, 
-                                  processed_data_points, 
-                                  lNameEdit.text(), 
-                                  notes.toPlainText(), 
+                session = Session(fNameEdit.text(),
+                                  raw_data_points,
+                                  processed_data_points,
+                                  lNameEdit.text(),
+                                  notes.toPlainText(),
                                   None,
                                   stats.mean_maximum_force(),
                                   stats.mean_sustained_force(),
@@ -234,6 +239,7 @@ class UI(QtGui.QWidget):
         if progress == 0:
             self.conn_worker.record = True
         if progress == 100:
+            msgBox.show()
             self.conn_worker.record = False
 
 #This class is used to multithread the timer, so it doesn't freeze the UI.
@@ -310,16 +316,15 @@ class ProcessingWorking(QtCore.QThread):
         self.data = data
         angle_f = [float(x) for x in angle.split(', ')]
         quo = angle_f[5] / angle_f[6]
-        self.angle = np.rad2deg(np.tan(quo))
+        self.angle = np.arctan(quo)
         self.p_data = None
-        print "Angle tan(%d/%d (%f)): %f" % (angle_f[5], angle_f[6], quo, self.angle)
+        print "Angle tan(%d/%d (%f)): %f" % (angle_f[5], angle_f[6], quo, np.rad2deg(self.angle))
 
     def run(self):
         # Process out of raw format
         arr = []
         for row in self.data.split('\n'):
             try:
-                print row
                 cols = [int(x) for x in row.split(', ')] # Converts columns to ints
                 if len(cols) == 15:
                     arr.append(cols)
